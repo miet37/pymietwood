40b5e4b9b1035381afb64c8404166d8ed20916ff
miet37 <53107758+miet37@users.noreply.github.com>
2020-10-12 20:46:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "<html><body><p><h1>Janusz ... to dla Ciebie,</h1></p><p>Hello, World! - pierwsza app na Azure</p></body></html>"
